02402adc3c686013a5870c5cdaf31ff686968479
lotaviods <l.otavio9099@gmail.com>
2021-01-16 16:07:13 -0300

    criação de diretiva para verificação de exclusão e estilo novo

---
+++ HTML/app/js/services/listagemService.js
@@ -0,0 +1,16 @@
+angular.module('main').service('listagemService', ['$http', '$rootScope', 'messageService', function($http, $rootScope, messageService) {
+
+    this.listagem = function() {
+        $http.get('http://localhost:8080/api/contatos', {
+            method: 'GET'
+        }).success(resp => {
+            $rootScope.resp = resp;
+            $rootScope.contatos = $rootScope.resp.conteudoResposta;
+            if (!$rootScope.contatos.length)
+                messageService.insereMensagem('Não existe nenhum contato');
+        }).error(resp => {
+            messageService.insereMensagem('Ocorreu um erro na busca');
+        })
+
+    }
+}])
