3f4a695f17d13507ea4222c98ca4ca17130fac7d
lotaviods <l.otavio9099@gmail.com>
2021-01-16 21:48:31 -0300

    ADição de menu de edição e funcionalidade da mesma

---
+++ HTML/app/js/services/listagemService.js
@@ -0,0 +1,22 @@
+angular.module('main').service('listagemService', ['$http', '$rootScope', 'messageService', function($http, $rootScope, messageService) {
+
+    this.listagem = function(id) {
+        if (id == null) id = '';
+        $http.get(`http://localhost:8080/api/contatos/${id}`, {
+            method: 'GET'
+        }).success(resp => {
+            $rootScope.resp = resp;
+            $rootScope.contatos = $rootScope.resp.conteudoResposta;
+            if ($rootScope.contatos == null) {
+                messageService.insereMensagem('Não foi possivél efetuar a busca');
+                return;
+            }
+            if (resp.conteudoResposta.length == 0)
+                messageService.insereMensagem('Não encontramos nenhum contato');
+            $rootScope.$broadcast("success", resp);
+        }).error(resp => {
+            messageService.insereMensagem('Ocorreu um erro na busca');
+        })
+
+    }
+}])
