c71181b1ecba9e2a7364d58c3eb5d9f0b64145e0
lotaviods <l.otavio9099@gmail.com>
2021-01-16 22:07:38 -0300

    Adição de mimificação de css com gulp

--- HTML/gulpfile.js
+++ HTML/gulpfile.js
@@ -1,12 +1,24 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser');
-gulp.task('dev', function() {
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('js', function() {
     return gulp.src(['app/js/*.js', 'app/js/services/*.js', 'app/js/directives/*.js', 'app/js/controllers/*.js']) // get source files with gulp.src
         .pipe(concat('all.js'))
+        .pipe(sourcemaps.init())
         .pipe(terser({
             keep_fnames: false,
             mangle: true
         }))
+        .pipe(sourcemaps.write('/'))
         .pipe(gulp.dest('public/js'));
+});
+gulp.task('css', function() {
+    return gulp.src('app/css/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('/'))
+        .pipe(gulp.dest('public/css'));
 });
