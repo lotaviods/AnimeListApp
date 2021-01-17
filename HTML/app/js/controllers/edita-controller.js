360bdbe3b59f3b8159dc437ff42c66f84a6d378c
lotaviods <l.otavio9099@gmail.com>
2021-01-16 21:48:31 -0300

    ADição de menu de edição e funcionalidade da mesma

---
+++ HTML/app/js/controllers/edita-controller.js
@@ -0,0 +1,29 @@
+angular.module('main').controller('editaController', ['$scope', '$rootScope', '$location', 'editaService', 'listagemService', '$routeParams', function($scope, $rootScope, $location, editaService, listagemService, $routeParams) {
+    $rootScope.titulo = "Edição de contatos";
+    $scope.id = $routeParams.id;
+    $scope.nome = '';
+    $scope.email = '';
+    $scope.num = '';
+    listagemService.listagem($scope.id);
+    $rootScope.$on("success", function(event, data) {
+        //console.log(data);
+        $scope.nome = $rootScope.contatos.nome;
+        $scope.email = $rootScope.contatos.email;
+        $scope.num = $rootScope.contatos.numero;
+    })
+
+    $rootScope.Pesquisa = false;
+
+    $scope.cadastrar = function() {
+        $scope.data = {
+            'nome': $scope.nome,
+            'email': $scope.email,
+            'numero': $scope.num
+        };
+        editaService.edita($scope.id, $scope.data);
+
+    }
+    $scope.inicio = function() {
+        $location.path("/");
+    }
+}])
