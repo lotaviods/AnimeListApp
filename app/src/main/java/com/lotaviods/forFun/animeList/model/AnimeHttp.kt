8c269174f0935ad0ee7a6a37281e946dd3756ecb
Luiz Otávio <luiz.otavio@ftdata.com.br>
2021-09-12 01:44:03 -0300

    feat:(Buildi): Adiciona API no build config

--- app/src/main/java/com/lotaviods/forFun/animeList/model/AnimeHttp.kt
+++ app/src/main/java/com/lotaviods/forFun/animeList/model/AnimeHttp.kt
@@ -1,18 +1,19 @@
 package com.lotaviods.forFun.animeList.model
 
 import com.google.gson.Gson
+import com.lotaviods.forFun.animeList.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
 object AnimeHttp {
-private const val URL = "https://lotaviodsanimelist.herokuapp.com/api/anime"
-    //https://lotaviods.netlify.app/.netlify/functions/api/anime
-    private val client =  OkHttpClient().newBuilder()
+    private const val URL = BuildConfig.API_URL
+
+    private val client = OkHttpClient().newBuilder()
         .readTimeout(10, TimeUnit.SECONDS)
         .connectTimeout(5, TimeUnit.SECONDS)
         .build()
+
     fun searchAnimes(): InfoRequest? {
         return try {
             val request = Request.Builder().url(URL)
@@ -20,8 +21,10 @@ private const val URL = "https://lotaviodsanimelist.herokuapp.com/api/anime"
             val response = client.newCall(request).execute()
             val json = response.body?.string()
             Gson().fromJson(json, InfoRequest::class.java)
-        }catch (e: Exception){
+        } catch (e: Exception) {
             null
         }
     }
+
+
 }
